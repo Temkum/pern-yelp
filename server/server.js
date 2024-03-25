51af73b6b20349fb269bc35e71c4c585f6333a9f
Kum Jude Tem <judekum14@gmail.com>
2024-03-25 01:27:34 +0100

    add routes and middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -7,9 +8,57 @@ const app = new express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from server!');
-});
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express.json());
+app.use(morgan('dev'));
 
-export default app;
+app.use((req, res, next) => {
+  console.log('Middleware running...');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Second middleware...');
+  next();
+});
+
+app.get('/api/v1/restaurants', (req, res) => {
+  res.status(200).json({
+    status: 'SUCCESS',
+    data: {
+      restaurant: ['exa1', 'exam2', 'exam3'],
+    },
+  });
+});
+
+app.post('/api/v1/restaurants', (req, res) => {
+  console.log(req.body);
+});
+
+app.get('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: 'SUCCESS',
+    data: {
+      restaurant: 'chicken',
+    },
+  });
+});
+
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id);
+
+  res.status(200).json({
+    status: 'SUCCESS',
+    data: {
+      restaurant: 'bread',
+    },
+  });
+});
+
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id);
+  res.status(204).send('DELETED');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
